67264b493de3a1f3f57c619eb71382e555fe1df7
Marcos Gomes <marcosgomesdev@gmail.com>
2024-05-04 15:59:56 -0300

    feat: create loading component

---
+++ src/components/ActivityIndicator/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  ActivityIndicatorProps,
+  ActivityIndicator as RNActivityIndicator,
+} from 'react-native';
+
+import { useAppTheme } from '@hooks';
+import { ThemeColors } from '@theme';
+
+interface Props extends Omit<ActivityIndicatorProps, 'color'> {
+  color?: ThemeColors;
+}
+export function ActivityIndicator({ color = 'primary', ...rest }: Props) {
+  const { colors } = useAppTheme();
+
+  return (
+    <RNActivityIndicator
+      testID="activity-indicator"
+      color={colors[color]}
+      {...rest}
+    />
+  );
+}
