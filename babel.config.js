247eb88603c8557be8a1b0d16509f0c9872e39a4
Marcos Gomes <marcosgomesdev@gmail.com>
2024-05-04 15:52:45 -0300

    config absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@hooks': './src/hooks',
+          '@utils': './src/utils',
+          '@types': './src/types',
+          '@routes': './src/routes',
+          '@styles': './src/styles',
+          '@assets': './src/assets',
+          '@theme': './src/theme',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@components': './src/components',
+          '@interfaces': './src/interfaces',
+        },
+      },
+    ],
+  ],
 };
