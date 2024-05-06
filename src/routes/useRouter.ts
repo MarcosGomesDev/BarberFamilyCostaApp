28e8b4510c3e22a639a980616a640d61a6e61474
Marcos Gomes <marcosgomesdev@gmail.com>
2024-05-06 01:58:42 -0300

    feat create useRouter flow function

---
+++ src/routes/useRouter.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+
+import { settingsService, useAuthCredentials } from '@services';
+
+export type Stacks = 'Loading' | 'Auth' | 'App' | 'Onboarding';
+
+export function useRouter(): Stacks {
+  // const showOnboarding = useShowOnboarding();
+  const { authCredentials, isLoading } = useAuthCredentials();
+
+  useEffect(() => {
+    if (!isLoading) {
+      settingsService.hideSplashScreen();
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return 'Loading';
+  }
+
+  // if (showOnboarding) {
+  //   return 'Onboarding';
+  // }
+
+  if (!authCredentials) {
+    return 'App';
+  }
+
+  return 'Auth';
+}
