78a8e35aeccb656f4e6ab2a701ce94f56e1e0ac2
Marcos Gomes <marcosgomesdev@gmail.com>
2024-05-06 01:49:42 -0300

    feat: add navigation back

--- src/screens/app/Home/components/Recommendeds/index.tsx
+++ src/screens/app/Home/components/Recommendeds/index.tsx
@@ -1,13 +1,16 @@
 import { BarberShopCard, Box, Text } from '@components';
 import { BarberShop } from '@interfaces';
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { FlatList } from 'react-native';
 
-interface Recommendeds {
+interface RecommendedsProps {
   data: BarberShop[];
 }
 
-export function Recommendeds({ data }: Recommendeds) {
+export function Recommendeds({ data }: RecommendedsProps) {
+  const navigation = useNavigation();
+
   return (
     <Box mt="s16" pt="s20" mb="s10">
       <Text color="gray3" paddingLeft="s16" bold>
@@ -24,6 +27,7 @@ export function Recommendeds({ data }: Recommendeds) {
         ItemSeparatorComponent={() => <Box width={15} />}
         renderItem={({ item, index }) => (
           <BarberShopCard
+            onPress={() => navigation.navigate('BarbershopDetailsScreen')}
             data={item}
             style={{
               marginLeft: index === 0 ? 16 : 0,
