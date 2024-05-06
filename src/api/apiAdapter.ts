e42b1e3d3bf13afbf3c30312a12c6d42aa9539d1
Marcos Gomes <marcosgomesdev@gmail.com>
2024-05-06 02:03:52 -0300

    feat: create api adapter and configs

---
+++ src/api/apiAdapter.ts
@@ -0,0 +1,30 @@
+import { MetaDataPage, Page } from '@types';
+
+import { MetaDataPageAPI, PageAPI } from './apiTypes';
+
+function toMetaDataPage(meta: MetaDataPageAPI): MetaDataPage {
+  return {
+    total: meta.total,
+    perPage: meta.per_page,
+    currentPage: meta.current_page,
+    lastPage: meta.last_page,
+    firstPage: meta.first_page,
+    hasNextPage: !!meta.next_page_url,
+    hasPreviousPage: !!meta.previous_page_url,
+  };
+}
+
+function toPageModel<ApiType, ModelType>(
+  page: PageAPI<ApiType>,
+  adapterToModel: (api: ApiType) => ModelType,
+): Page<ModelType> {
+  return {
+    meta: toMetaDataPage(page.meta),
+    data: page.data.map(adapterToModel),
+  };
+}
+
+export const apiAdapter = {
+  toMetaDataPage,
+  toPageModel,
+};
---
+++ src/api/apiConfig.ts
@@ -0,0 +1,57 @@
+import { AuthCredentials, authService } from '@domain';
+import axios from 'axios';
+
+/**
+ * use your computer network IP Address when running on a real device.
+ *
+ * i.e: `'http://192.168.20.15:3333/'`
+ */
+export const BASE_URL = 'http://127.0.0.1:3333/';
+export const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+type InterceptorProps = {
+  authCredentials: AuthCredentials | null;
+  saveCredentials: (ac: AuthCredentials) => Promise<void>;
+  removeCredentials: () => Promise<void>;
+};
+
+export function registerInterceptor({
+  authCredentials,
+  removeCredentials,
+  saveCredentials,
+}: InterceptorProps) {
+  const interceptor = api.interceptors.response.use(
+    response => response,
+    async responseError => {
+      const failedRequest = responseError.config;
+      const hasNotRefreshToken = !authCredentials?.refreshToken;
+      const isRefreshTokenRequest =
+        authService.isRefreshTokenRequest(failedRequest);
+
+      if (responseError.response.status === 401) {
+        if (hasNotRefreshToken || isRefreshTokenRequest || failedRequest.sent) {
+          removeCredentials();
+          return Promise.reject(responseError);
+        }
+
+        failedRequest.sent = true;
+
+        const newAuthCredentials = await authService.authenticateByRefreshToken(
+          authCredentials?.refreshToken,
+        );
+        saveCredentials(newAuthCredentials);
+
+        failedRequest.headers.Authorization = `Bearer ${newAuthCredentials.token}`;
+
+        return api(failedRequest);
+      }
+
+      return Promise.reject(responseError);
+    },
+  );
+
+  // remove listener when component unmount
+  return () => api.interceptors.response.eject(interceptor);
+}
