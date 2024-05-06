cef96eee1e7aa7cff16b756845681daee9a808f8
Marcos Gomes <marcosgomesdev@gmail.com>
2024-05-06 02:02:18 -0300

    feat: add settings and search history service

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+export * from './authCredentials';
+export * from './searchHistory';
+export * from './settings';
+export * from './storage';
+export * from './toast';
---
+++ src/services/settings/settingsService.ts
@@ -0,0 +1,59 @@
+import { Appearance, ColorSchemeName, Platform, StatusBar } from 'react-native';
+
+import BootSplash from 'react-native-bootsplash';
+
+import { palette } from '@theme';
+
+import { AppColorScheme, ThemePreference } from './settingsType';
+
+function onChangeThemePreference(
+  themePreference: ThemePreference,
+): AppColorScheme {
+  if (themePreference === 'system') {
+    const colorScheme = Appearance.getColorScheme();
+    return colorScheme ? colorScheme : 'light';
+  }
+
+  return themePreference;
+}
+
+function onSystemChange(
+  color: ColorSchemeName,
+  themePreference: ThemePreference,
+): AppColorScheme | null {
+  if (themePreference === 'system') {
+    return color ? color : 'light';
+  }
+  return null;
+}
+
+function handleStatusBar(appColor: AppColorScheme) {
+  StatusBar.setBarStyle(
+    appColor === 'dark' ? 'light-content' : 'dark-content',
+    true,
+  );
+
+  if (Platform.OS === 'android') {
+    StatusBar.setBackgroundColor(
+      appColor === 'dark' ? palette.backgroundBlack : palette.darkPurple,
+    );
+  }
+}
+
+async function hideSplashScreen() {
+  try {
+    const isVisible = await BootSplash.isVisible();
+    if (isVisible) {
+      BootSplash.hide({ fade: true });
+    }
+  } catch (error) {
+    BootSplash.hide();
+  }
+}
+
+export const settingsService = {
+  onChangeThemePreference,
+  onSystemChange,
+  handleStatusBar,
+  hideSplashScreen,
+};
