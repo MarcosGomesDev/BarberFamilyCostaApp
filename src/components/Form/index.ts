dc92ddb2a31971fec9d2786c71c709086e050b92
Marcos Gomes <marcosgomesdev@gmail.com>
2025-04-10 02:21:56 -0300

    feat: add components

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -2,6 +2,7 @@ import { default as FormCurrencyInput } from './FormCurrencyInput';
 import { default as FormOTPInput } from './FormOTPInput';
 import { default as FormPasswordInput } from './FormPasswordInput';
 import { default as FormTextInput } from './FormTextInput';
+export * from './FormDatePickerInput';
 export * from './FormSelectInput';
 
 export { FormCurrencyInput, FormOTPInput, FormPasswordInput, FormTextInput };
